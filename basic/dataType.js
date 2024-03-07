18034ad4c4eecfa33bf6cc227e04c589bfc505ff
Md Sujon Miah <mdsujon72@gmail.com>
2024-03-08 04:52:48 +0600

    first commit

---
+++ basic/dataType.js
@@ -0,0 +1,7 @@
+"use strike"; //treat alljs codeas newerversion
+
+console.log(3+3); console.log(6-7);
+console.log("zamir");
+console.log("mizan");
+console.log("zazamal")
+const personName = "zamir";
---
+++ basic/variable.js
@@ -0,0 +1,6 @@
+const accountId = 144553;
+let accountEmail = "mdsujon72@gmail.com";
+var accountPassword = "12345";
+let accountCity = "dhaka";
+
+console.table(accountId, accountEmail, accountPassword, accountCity )
